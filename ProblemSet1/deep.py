c4443fa42bf1bc1995f8cf9893565e4c5916ec6e
ingrydc <ingrydcamilly15@gmail.com>
2024-12-06 14:17:53 -0300

    ex01

---
+++ ProblemSet1/deep.py
@@ -0,0 +1,6 @@
+answer = input('What is the answer to life, the universe, and everything? ')
+
+if answer == '42' or answer == 'forty two' or answer == 'forty-two':
+    print('Yes')
+else:
+    print('No')
